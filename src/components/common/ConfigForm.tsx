2e95768872af5262c73e7edc4b1007846d4562a7
Dubian Sepulveda <66980518+dubian26@users.noreply.github.com>
2026-02-09 19:02:16 -0500

    Se agregan validacion para los parametros de configuración

--- src/components/common/ConfigForm.tsx
+++ src/components/common/ConfigForm.tsx
@@ -1,6 +1,7 @@
 import { Button } from "primereact/button"
 import { InputText } from "primereact/inputtext"
 import { Fragment, useContext, useEffect, useState } from "react"
+import { convert } from "../../appconfig/Convert"
 import { AppContext } from "../../contexts/AppContext"
 import { type ConfigModel } from "../../models/ConfigModel"
 
@@ -34,7 +35,80 @@ export const ConfigForm = ({ onSave }: Props) => {
    const handleSave = async () => {
       try {
          if (!changed) {
-            mostrarMensaje("No hay cambios para guardar")
+            mostrarError("No hay cambios para guardar")
+            return
+         }
+
+         const regex = /^\d+[smhd]$/i
+         const valorMockRequestDelay = formData.mockRequestDelay?.toString() ?? ""
+         const valorExpAccessToken = formData.expAccessToken?.toString() ?? ""
+         const valorExpRefreshToken = formData.expRefreshToken?.toString() ?? ""
+         const valorSessionTimeout = formData.sessionTimeout?.toString() ?? ""
+         const valorSessionWarning = formData.sessionWarning?.toString() ?? ""
+
+         if (!regex.test(valorMockRequestDelay)) {
+            mostrarError("El valor de 'tiempo en http requests' no es valido. Ej: 1s, 2m, 3h, 4d")
+            return
+         }
+
+         if (!regex.test(valorExpAccessToken)) {
+            mostrarError("El valor de 'expiración access token' no es valido. Ej: 1s, 2m, 3h, 4d")
+            return
+         }
+
+         if (!regex.test(valorExpRefreshToken)) {
+            mostrarError("El valor de 'expiración refresh token' no es valido. Ej: 1s, 2m, 3h, 4d")
+            return
+         }
+
+         if (!regex.test(valorSessionTimeout)) {
+            mostrarError("El valor de 'tiempo espera para cerrar sesion por inactividad' no es valido. Ej: 1s, 2m, 3h, 4d")
+            return
+         }
+
+         if (!regex.test(valorSessionWarning)) {
+            mostrarError("El valor de 'avisar antes de cerrar sesion' no es valido. Ej: 1s, 2m, 3h, 4d")
+            return
+         }
+
+         const mockDelay = convert.toSeconds(valorMockRequestDelay)
+         const expAccessToken = convert.toSeconds(valorExpAccessToken)
+         const expRefreshToken = convert.toSeconds(valorExpRefreshToken)
+         const sessionTimeout = convert.toSeconds(valorSessionTimeout)
+         const sessionWarning = convert.toSeconds(valorSessionWarning)
+
+         if (mockDelay > 5) {
+            mostrarError("El valor de 'tiempo en http requests' no debe superar los 5 segundos")
+            return
+         }
+
+         if (expAccessToken > 1800) {
+            mostrarError("El valor de 'expiración access token' no debe superar los 30 minutos")
+            return
+         }
+
+         if (expRefreshToken > 172800) {
+            mostrarError("El valor de 'expiración refresh token' no debe superar los 2 días")
+            return
+         }
+
+         if (expAccessToken >= expRefreshToken) {
+            mostrarError("El valor de 'expiración access token' debe ser menor que el valor de 'expiración refresh token'")
+            return
+         }
+
+         if (sessionTimeout > 1800) {
+            mostrarError("El valor de 'tiempo espera para cerrar sesion por inactividad' no debe superar los 30 minutos")
+            return
+         }
+
+         if (sessionWarning > 300) {
+            mostrarError("El valor de 'avisar antes de cerrar sesion' no debe superar los 5 minutos")
+            return
+         }
+
+         if (sessionWarning >= sessionTimeout) {
+            mostrarError("El valor de 'avisar antes de cerrar sesion' debe ser menor que el valor de 'tiempo espera para cerrar sesion por inactividad'")
             return
          }
 
@@ -43,6 +117,7 @@ export const ConfigForm = ({ onSave }: Props) => {
          mostrarMensaje("Configuración actualizada correctamente")
          setChanged(false)
          onSave()
+
       } catch (error) {
          console.error(error)
          mostrarError("Error al guardar la configuración")
@@ -74,7 +149,7 @@ export const ConfigForm = ({ onSave }: Props) => {
                </label>
                <InputText
                   id="mockDelay" value={formData.mockRequestDelay}
-                  onChange={(e) => handleChange("mockRequestDelay", e.target.value)}
+                  onChange={e => handleChange("mockRequestDelay", e.target.value)}
                   className="w-full"
                />
             </div>
@@ -85,7 +160,7 @@ export const ConfigForm = ({ onSave }: Props) => {
                </label>
                <InputText
                   id="expAccess" value={formData.expAccessToken}
-                  onChange={(e) => handleChange("expAccessToken", e.target.value)}
+                  onChange={e => handleChange("expAccessToken", e.target.value)}
                   className="w-full"
                />
             </div>
@@ -96,7 +171,7 @@ export const ConfigForm = ({ onSave }: Props) => {
                </label>
                <InputText
                   id="expRefresh" value={formData.expRefreshToken}
-                  onChange={(e) => handleChange("expRefreshToken", e.target.value)}
+                  onChange={e => handleChange("expRefreshToken", e.target.value)}
                   className="w-full"
                />
             </div>
@@ -107,7 +182,7 @@ export const ConfigForm = ({ onSave }: Props) => {
                </label>
                <InputText
                   id="sessionTimeout" value={formData.sessionTimeout}
-                  onChange={(e) => handleChange("sessionTimeout", e.target.value)}
+                  onChange={e => handleChange("sessionTimeout", e.target.value)}
                   className="w-full"
                />
             </div>
@@ -118,7 +193,7 @@ export const ConfigForm = ({ onSave }: Props) => {
                </label>
                <InputText
                   id="sessionWarning" value={formData.sessionWarning}
-                  onChange={(e) => handleChange("sessionWarning", e.target.value)}
+                  onChange={e => handleChange("sessionWarning", e.target.value)}
                   className="w-full"
                />
             </div>
